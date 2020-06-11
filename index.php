c71d667e15c7942bbebf238b1f18bd7082380ccf
Alexander Obuhovich <aik.bold@gmail.com>
2020-06-11 10:33:37 +0300

    Connect to a specified URL and show connection statistics (aka ping)

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+if (!array_key_exists('PING_URL', $_SERVER) || empty($_SERVER['PING_URL'])) {
+    exit;
+}
+
+$start_time = time();
+
+$url = $_SERVER['PING_URL'];
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+curl_setopt($ch, CURLOPT_TIMEOUT, 90);
+
+curl_setopt($ch, CURLOPT_REFERER, 'https://www.google.com');
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36');
+
+$ret = curl_exec($ch);
+$end_time = time();
+
+echo 'URL: ' . $url . PHP_EOL;
+echo 'HTTP Code: ' . curl_getinfo($ch, CURLINFO_HTTP_CODE) . PHP_EOL;
+echo 'Length: ' . strlen($ret) . ' bytes' . PHP_EOL;
+echo 'Duration: ' . ($end_time - $start_time) . ' sec' . PHP_EOL;
+
+curl_close($ch);
